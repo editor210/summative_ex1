23e9df216ea849b5bed7ae8ad73e28cf5ef6ab5e
editor210 <edimilson.torres@gmail.com>
2023-10-23 19:33:56 +0100

    Transform Time Series into DF

---
+++ data_transformation.R
@@ -0,0 +1,26 @@
+#get the AirPassengers dataset from R and transform the time series into a tidy data frame
+library(tidyverse)
+
+data <- AirPassengers
+
+data_transform <- function(data){
+                                 #transforming the data variable into a dataframe
+                                 #and the values as integers
+                                 data <- as.data.frame(as.integer(data))
+  
+                                 #Assigning the name of the column
+                                 colnames(data) <- "Passengers"
+                                 #Including the Years and Months columns
+                                 data <- data %>% mutate(Year = rep(seq(1949,1960), each = 12),
+                                                         Month = rep(c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"), 12))
+                                
+                                 #Rearranging the columns to make it more readable
+                                 data <- data[c("Year", "Month", "Passengers")]
+  
+                                 return(data)
+
+                                 }
+
+
+test <- data_transform(AirPassengers)
+test
